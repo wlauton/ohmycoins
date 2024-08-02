1bb559ca7be22fe119fac878284c2e4de1313264
Wlauton Burity <wlauton@live.fr>
2024-08-01 21:17:33 -0300

    Função para conversão de valores.

--- script.js
+++ script.js
@@ -34,10 +34,35 @@ document.addEventListener('DOMContentLoaded', function() {
             console.error('Erro ao buscar taxas de câmbio:', error);
         });
 });
+
 /**
  * Função para manipular o envio do formulário de conversão de moeda.
+ * @param {Event} event - O evento de submissão do formulário.
  */
 document.getElementById('conversor-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Previne o comportamento padrão do formulário
 
+    // Seleciona os elementos dropdown e o valor de entrada
+    const entradaMoedaSelect = document.getElementById('entrada-conversor');
+    const saidaMoedaSelect = document.getElementById('saida-conversor');
+    let valor = document.getElementById('quantia').value.replace(',', '.'); // Normaliza o valor para float
+    let entradaMoeda = entradaMoedaSelect.value;
+    let saidaMoeda = saidaMoedaSelect.value;
+
+    // Faz uma requisição à API Exchange Rate para obter a taxa de câmbio da moeda de entrada
+    fetch(`https://api.exchangerate-api.com/v4/latest/${entradaMoeda}`)
+        .then(response => response.json())
+        .then(data => {
+            let taxa = data.rates[saidaMoeda]; // Obtém a taxa de câmbio para a moeda de saída
+            let conversao = valor * taxa; // Realiza a conversão
+
+            // Exibe o resultado da conversão
+            document.getElementById('resultado').style.visibility = "visible";
+            document.getElementById('resultado').innerText = ` ${conversao.toFixed(2)} ${saidaMoeda}`;
+        })
+        .catch(error => {
+            // Loga um erro caso a requisição falhe e exibe uma mensagem de erro para o usuário
+            console.error('Erro ao buscar taxa de câmbio:', error);
+            document.getElementById('resultado').innerText = 'Erro ao buscar taxa de câmbio. Tente novamente.';
+        });
 });
